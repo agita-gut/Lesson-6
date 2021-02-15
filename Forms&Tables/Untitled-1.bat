8cdb6c2fdc306fad38fbe1c2f78afac5cfb251e9
agita-gut <78403044+agita-gut@users.noreply.github.com>
2021-02-15 21:32:10 +0200

    Add some text

---
+++ Forms&Tables/Untitled-1.bat
@@ -0,0 +1,15 @@
+file:///C:/Users/Agita/Documents/GitHub/Lesson-6/forms.html?
+name=Foo+Bar
+&password=asdfghjk
+&gender=on
+&country=Estonia
+&coment=++++text+text+text+%0D%0A++++%0D%0A++++%0D%0A++++%0D%0A++++text+text+text+text+text%0D%0A+++++text+text+text+%0D%0A+++++text%0D%0Accc%0D%0A
+
+LF - mac = %0A =/n uz otru pusi slash
+CRLF - new line for windows = %0D%OA = /r/n uz otru pusi slash
+
+
+%0D
+%0A 
+
+protocol - domain name - resource - url params - hash params 
